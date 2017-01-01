add matrix solver - maybe not necssary, also fixed implicit derivative equations

code_submission_nonsmooth/common.py
@@ -63,7 +63,13 @@ def testerror_matrix_completion(data, indices, model_params):
         model_params["interaction_m"]
     )
     # index column-major style
-    return 0.5/indices.size * get_norm2(data.observed_matrix.flatten('F').A[0][indices] - fitted_m.flatten('F').A[0][indices], power=2)
+    return 0.5/indices.size * get_norm2(make_column_major_flat(data.observed_matrix - fitted_m)[indices], power=2)
+
+def make_column_major_flat(m):
+    return np.reshape(m, (m.size, 1), order='F')
+
+def make_column_major_reshape(m, shape):
+    return np.reshape(m, shape, order='F')
 
 def get_matrix_completion_fitted_values(row_feat, col_feat, row_theta, col_theta, interaction_m):
     num_rows = row_feat.shape[0]

code_submission_nonsmooth/convexopt_solvers.py
@@ -4,6 +4,7 @@ import cvxopt
 from common import *
 import scipy as sp
 import cvxpy
+from matrix_completion_solver import MatrixCompletionProblem
 
 SCS_MAX_ITERS = 10000
 SCS_EPS = 1e-3 # default eps
@@ -677,7 +678,36 @@ class SparseAdditiveModelProblemWrapper:
                 print "Warning: Negative problem solution from cvxpy"
             return None
 
+class MatrixCompletionProblemWrapper:
+    def __init__(self, data, tiny_e=0):
+        assert(tiny_e == 0)
+        self.problem = MatrixCompletionProblem(data)
+    def solve(self, lambdas, warm_start=True, quick_run=False):
+        row_theta, col_theta, interaction_m = self.problem.solve(lambdas)
+        return {
+            "row_theta": row_theta,
+            "col_theta": col_theta,
+            "interaction_m": interaction_m
+        }
+
+class MatrixCompletionProblemWrapperStupid:
+    def __init__(self, data, tiny_e=0):
+        assert(tiny_e == 0)
+        self.problem = MatrixCompletionProblem(data)
+    def solve(self, lambdas, warm_start=True, quick_run=False):
+        exploded_lambdas = np.array([lambdas[0]] + [lambdas[1]] * 4)
+        print "exploded_lambdas", exploded_lambdas
+        self.problem.update(exploded_lambdas)
+        interaction_m, row_theta, col_theta = self.problem.solve()
+        return {
+            "row_theta": row_theta,
+            "col_theta": col_theta,
+            "interaction_m": interaction_m
+        }
+
+
 class MatrixCompletionProblemWrapperSimple:
+    # This is not accurate enough. This should be deprecated
     # Suppose one parameter for the nuclear norm and one for the two lasso penalties
     # @param data: should be a MatrixObservedData
     def __init__(self, data, tiny_e=0):
@@ -695,17 +725,17 @@ class MatrixCompletionProblemWrapperSimple:
         objective = (
             0.5/num_train * sum_squares(
                 self._get_train_idx(
-                    data.observed_matrix -
-                    (
-                        data.row_features * self.row_theta * np.matrix(np.ones(data.num_rows))
-                        + (data.col_features * self.col_theta * np.matrix(np.ones(data.num_cols))).T
-                        + self.interaction_m
-                    ),
+                    data.observed_matrix
+                    - data.row_features * self.row_theta * np.matrix(np.ones(data.num_rows))
+                    - (data.col_features * self.col_theta * np.matrix(np.ones(data.num_cols))).T
+                    - self.interaction_m,
                     data.train_idx
                 )
             ) + self.lambdas[0] * norm(self.interaction_m, "nuc")
-            + self.lambdas[1] * (pnorm(self.row_theta, 1) + 0.5 * pnorm(self.row_theta, 2))
-            + self.lambdas[1] * (pnorm(self.col_theta, 1) + 0.5 * pnorm(self.col_theta, 2))
+            + self.lambdas[1] * (
+                norm(self.row_theta, 1) + 0.5 * sum_squares(self.row_theta)
+                + norm(self.col_theta, 1) + 0.5 * sum_squares(self.col_theta)
+            )
         )
         # objective += 0.5/num_train * self.tiny_e * sum_squares(self.row_thetas)
         self.problem = Problem(Minimize(objective))
@@ -719,8 +749,8 @@ class MatrixCompletionProblemWrapperSimple:
         for i in range(lambdas.size):
             self.lambdas[i].value = lambdas[i]
 
-        eps = SCS_EPS/10000
-        max_iters = SCS_MAX_ITERS * 100
+        eps = SCS_EPS
+        max_iters = SCS_MAX_ITERS
 
         self.problem.solve(solver=SCS, verbose=VERBOSE, max_iters=max_iters, use_indirect=False, eps=eps, normalize=False, warm_start=warm_start)
         print "cvxpy solved: value, status:", self.problem.value, self.problem.status
@@ -730,40 +760,40 @@ class MatrixCompletionProblemWrapperSimple:
             "interaction_m": self.interaction_m.value
         }
 
-class MatrixTest:
-    def __init__(self, obs_matrix):
-        self.lambda0 = Parameter(sign="positive")
+# class MatrixTest:
+#     def __init__(self, obs_matrix):
+#         self.lambda0 = Parameter(sign="positive")
+#
+#         self.gamma = Variable(3, 3)
+#
+#         objective = 0.5 * sum_squares(obs_matrix - self.gamma) + self.lambda0 * norm(self.gamma, "nuc")
+#         self.problem = Problem(Minimize(objective))
+#
+#     def solve(self, lambda0, warm_start=True, quick_run=False):
+#         start_time = time.time()
+#         self.lambda0.value = lambda0
+#
+#         eps = 1e-9
+#         max_iters = 10000
+#
+#         self.problem.solve(solver=SCS, verbose=VERBOSE, max_iters=max_iters, use_indirect=False, eps=eps, normalize=False, warm_start=warm_start)
+#         return self.gamma.value
 
-        self.gamma = Variable(3, 3)
-
-        objective = 0.5 * sum_squares(obs_matrix - self.gamma) + self.lambda0 * norm(self.gamma, "nuc")
-        self.problem = Problem(Minimize(objective))
-
-    def solve(self, lambda0, warm_start=True, quick_run=False):
-        start_time = time.time()
-        self.lambda0.value = lambda0
-
-        eps = 1e-9
-        max_iters = 10000
-
-        self.problem.solve(solver=SCS, verbose=VERBOSE, max_iters=max_iters, use_indirect=False, eps=eps, normalize=False, warm_start=warm_start)
-        return self.gamma.value
-
-class FeasibleTest:
-    def __init__(self, A, B):
-        self.m1 = Symmetric(3,3)
-        self.m2 = Variable(3,3)
-
-        constraints = [
-            A * self.m1 + self.m2 * B == 0,
-            B * self.m1 + self.m2 * A == 0,
-            self.m2 + self.m1 == B
-        ]
-        self.problem = Problem(Minimize(0), constraints)
-
-    def solve(self, warm_start=True, quick_run=False):
-        print self.problem.solve()
-        return self.problem.status, self.problem.value, self.m1.value, self.m2.value
+# class FeasibleTest:
+#     def __init__(self, A, B):
+#         self.m1 = Symmetric(3,3)
+#         self.m2 = Variable(3,3)
+#
+#         constraints = [
+#             A * self.m1 + self.m2 * B == 0,
+#             B * self.m1 + self.m2 * A == 0,
+#             self.m2 + self.m1 == B
+#         ]
+#         self.problem = Problem(Minimize(0), constraints)
+#
+#     def solve(self, warm_start=True, quick_run=False):
+#         print self.problem.solve()
+#         return self.problem.status, self.problem.value, self.m1.value, self.m2.value
 
 def _make_discrete_diff_matrix_ord2(x_features):
     num_samples = len(x_features)

code_submission_nonsmooth/data_generator.py
@@ -112,12 +112,12 @@ class DataGenerator:
             np.zeros(self.settings.num_row_features - self.settings.num_nonzero_row_features)
         ))).T
         col_beta = np.matrix(np.concatenate((
-            2 * np.ones(self.settings.num_nonzero_col_features),
+            np.ones(self.settings.num_nonzero_col_features),
             np.zeros(self.settings.num_col_features - self.settings.num_nonzero_col_features)
         ))).T
 
         row_corr_matrix = _make_correlation_matrix(0.5, self.settings.num_row_features)
-        col_corr_matrix = _make_correlation_matrix(0.7, self.settings.num_col_features)
+        col_corr_matrix = _make_correlation_matrix(0.5, self.settings.num_col_features)
 
         row_features = np.matrix(np.random.randn(self.settings.num_rows, self.settings.num_row_features)) * row_corr_matrix
         col_features = np.matrix(np.random.randn(self.settings.num_cols, self.settings.num_col_features)) * col_corr_matrix

code_submission_nonsmooth/gradient_descent_algo.py
@@ -159,35 +159,33 @@ class Gradient_Descent_Algo:
 
         return np.maximum(current_lambdas - new_step_size * lambda_derivatives, self.lambda_mins)
 
-    def _double_check_derivative(self, calculated_derivative, accept_diff=1e-1, epsilon=1e-5):
+    def _double_check_derivative_indepth(self, i, model1, model2, model0, eps):
+        return
+
+    def _double_check_derivative(self, calculated_derivative, accept_diff=1e-1, epsilon=1e-6):
         # Returns the numeral derivative if you want it
         deriv = []
         num_lambdas = len(self.fmodel.current_lambdas)
-        for i in range(1, num_lambdas):
+        print "self.fmodel.current_lambdas", self.fmodel.current_lambdas
+        for i in range(num_lambdas):
             print "===========CHECK I= %d ===============" % i
             # don't allow the discrete derivative perturb too much if the lambda value is low already
             eps = min(epsilon, self.fmodel.current_lambdas[i]/100)
             reg1 = np.copy(self.fmodel.current_lambdas)
             reg1[i] += eps
-            model1 = self.problem_wrapper.solve(np.array(reg1), quick_run=False) #, warm_start=False)
+            model1 = self.problem_wrapper.solve(np.array(reg1), quick_run=False)
             error1 = self.get_validate_cost(model1)
 
             reg2 = np.copy(self.fmodel.current_lambdas)
             reg2[i] -= eps
-            model2 = self.problem_wrapper.solve(np.array(reg2), quick_run=False) #, warm_start=False)
+            model2 = self.problem_wrapper.solve(np.array(reg2), quick_run=False)
             error2 = self.get_validate_cost(model2)
             i_deriv = (error1 - error2)/(eps * 2)
 
-            # REMOVE LATER?
-            print "dalpha_dlambda[i], i=%s, %s" % (i, (model1["row_theta"] - model2["row_theta"])/(eps * 2))
-            print "dBeta_dlambda[i], i=%s, %s" % (i, (model1["col_theta"] - model2["col_theta"])/(eps * 2))
+            model0 = self.problem_wrapper.solve(self.fmodel.current_lambdas, quick_run=False)
+            error0 = self.get_validate_cost(model0)
 
-            # gamma1 = model1["interaction_m"]
-            # u1, s1, v1 = np.linalg.svd(gamma1)
-            # gamma2 = model2["interaction_m"]
-            # u2, s2, v2 = np.linalg.svd(gamma2)
-            # print "du_dlambda[i], i=%s, %s" % (i, (u1 - u2)/(eps * 2))
-            # print "dgamma_dlambda[i], i=%s, %s" % (i, (gamma1 - gamma2)/(eps * 2))
+            self._double_check_derivative_indepth(i, model1, model2, model0, eps)
 
             # print "numerical sum_dthetas_dlambda", np.sum((np.concatenate(model1) - np.concatenate(model2))/(eps * 2), axis=1)
             print "calculated_derivative[i]", calculated_derivative[i]

code_submission_nonsmooth/matrix_completion_eval.py
@@ -19,8 +19,8 @@ from common import *
 
 class Matrix_Completion_Settings(Simulation_Settings):
     results_folder = "results/matrix_completion"
-    num_rows = 2
-    num_cols = 2
+    num_rows = 5
+    num_cols = 5
     num_row_features = 2
     num_col_features = 2
     num_nonzero_row_features = 1
@@ -158,7 +158,7 @@ def fit_data_for_iter(iter_data):
     settings = iter_data.settings
 
     one_vec = np.ones(5)
-    initial_lambdas_set = [one_vec, one_vec * 1e-1]
+    initial_lambdas_set = [one_vec]
     if settings.big_init_set:
         1/0
         # other_one_vec = np.ones(settings.expert_num_groups + 1)
@@ -166,7 +166,7 @@ def fit_data_for_iter(iter_data):
         # initial_lambdas_set += [other_one_vec, other_one_vec * 1e-1]
 
     one_vec2 = np.ones(2)
-    simple_initial_lambdas_set = [one_vec2 * 0.001, one_vec2]
+    simple_initial_lambdas_set = [one_vec2 * 0.1]
     if settings.big_init_set:
         1/0
         # other_one_vec2 = np.ones(2)