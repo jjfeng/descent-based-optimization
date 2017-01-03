b01a0519035fc4d06d716912f610666a0024f4e6
Jean Feng <jean.feng@gmail.com>
2017-01-03 10:12:46 -0800

    update gs

--- code_submission_nonsmooth/matrix_completion_grid_search.py
+++ code_submission_nonsmooth/matrix_completion_grid_search.py
@@ -1,12 +1,13 @@
 from grid_search import Grid_Search
 from common import testerror_matrix_completion
-from convexopt_solvers import MatrixCompletionProblemWrapperSimple
+from convexopt_solvers import MatrixCompletionProblemWrapperSimple, MatrixCompletionProblemWrapperStupid
 
 class Matrix_Completion_Grid_Search(Grid_Search):
     method_label = "Matrix_Completion_Grid_Search"
 
     def _create_problem_wrapper(self):
-        self.problem_wrapper = MatrixCompletionProblemWrapperSimple(self.data)
+        # self.problem_wrapper = MatrixCompletionProblemWrapperSimple(self.data)
+        self.problem_wrapper = MatrixCompletionProblemWrapperStupid(self.data)
 
     def get_validation_cost(self, model_params):
         return testerror_matrix_completion(
