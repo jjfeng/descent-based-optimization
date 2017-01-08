update hillclimb to have correct gradients

code_submission_nonsmooth/common.py
@@ -91,17 +91,17 @@ def testerror_matrix_completion_groups(data, indices, model_params):
         model_params["gamma"]
     )
     # index column-major style
-    return 0.5/indices.size * get_norm2(make_column_major_flat(data.observed_matrix - fitted_m)[indices], power=2)
+    return 0.5/indices.size * get_norm2(
+        make_column_major_flat(data.observed_matrix - fitted_m)[indices],
+        power=2
+    )
 
 def get_matrix_completion_groups_fitted_values(row_feats, col_feats, alphas, betas, gamma):
-    num_rows = gamma.shape[0]
-    num_cols = gamma.shape[1]
-
     m = 0
     if len(row_feats) > 0:
-        m += np.hstack(row_feats) * np.vstack(alphas) * np.ones(num_cols).T
+        m += np.hstack(row_feats) * np.vstack(alphas) * np.ones(gamma.shape[1]).T
     if len(col_feats) > 0:
-        m += (np.hstack(col_feats) * np.vstack(betas) * np.ones(num_rows).T).T
+        m += (np.hstack(col_feats) * np.vstack(betas) * np.ones(gamma.shape[0]).T).T
     return gamma + m
 
 def betaerror(beta_real, beta_guess):

code_submission_nonsmooth/matrix_completion_eval.py
@@ -172,7 +172,8 @@ def fit_data_for_iter(iter_data):
     settings = iter_data.settings
 
     one_vec = np.ones(5)
-    initial_lambdas_set = [one_vec * 0.01]
+    one_vec[0] = 10
+    initial_lambdas_set = [one_vec * 0.1, one_vec * 0.01]
     if settings.big_init_set:
         1/0
         # other_one_vec = np.ones(settings.expert_num_groups + 1)
@@ -189,7 +190,7 @@ def fit_data_for_iter(iter_data):
 
     method = iter_data.settings.method
 
-    str_identifer = "%d_%d_%d_%d_%d_%d_%d_%d_%s_%d_%d" % (
+    str_identifer = "%d_%d_%d_%d_%d_%d_%d_%d_%s_%d" % (
         settings.num_rows,
         settings.num_cols,
         settings.num_nonzero_s,
@@ -199,7 +200,6 @@ def fit_data_for_iter(iter_data):
         settings.num_nonzero_col_features,
         settings.snr,
         method,
-        settings.big_init_set,
         iter_data.i,
     )
     log_file_name = "%s/tmp/log_%s.txt" % (settings.results_folder, str_identifer)

code_submission_nonsmooth/matrix_completion_groups_eval.py
@@ -19,14 +19,14 @@ from common import *
 
 class Matrix_Completion_Group_Settings(Simulation_Settings):
     results_folder = "results/matrix_completion_groups"
-    num_nonzero_s = 1
-    num_rows = 2
-    num_cols = 2
+    num_nonzero_s = 2
+    num_rows = 10
+    num_cols = 10
     num_row_groups = 5
     num_col_groups = 5
-    num_row_features = 2 # num features per group
-    num_col_features = 2 # num features per group
-    num_nonzero_row_groups = 1
+    num_row_features = 3 # num features per group
+    num_col_features = 3 # num features per group
+    num_nonzero_row_groups = 4
     num_nonzero_col_groups = 1
     train_perc = 0.5
     validate_perc = 0.3
@@ -175,7 +175,8 @@ def fit_data_for_iter(iter_data):
     settings = iter_data.settings
 
     one_vec = np.ones(1 + settings.num_row_groups + settings.num_col_groups)
-    initial_lambdas_set = [one_vec]
+    one_vec[0] = 10
+    initial_lambdas_set = [one_vec * 0.01]
     if settings.big_init_set:
         1/0
         # other_one_vec = np.ones(settings.expert_num_groups + 1)

code_submission_nonsmooth/matrix_completion_groups_hillclimb.py
@@ -4,7 +4,7 @@ import cvxopt
 import numpy as np
 import scipy as sp
 from common import VERBOSE
-from common import print_time
+from common import print_time, get_norm2
 from common import testerror_matrix_completion_groups, get_matrix_completion_groups_fitted_values
 from common import make_column_major_flat, make_column_major_reshape
 from gradient_descent_algo import Gradient_Descent_Algo
@@ -48,7 +48,7 @@ class Lamdba_Deriv_Problem_Wrapper:
             self.obj += sum_squares(uu) + sum_squares(vv)
 
         def _create_var_vec(vec):
-            Variable(vec.size, 1) if feat.size > 0 else None
+            return Variable(vec.size, 1) if vec.size > 0 else None
 
         self.dalphas_dlambda = [_create_var_vec(a) for a in alphas]
         self.dbetas_dlambda = [_create_var_vec(b) for b in betas]
@@ -59,6 +59,7 @@ class Lamdba_Deriv_Problem_Wrapper:
         # The problem with solving the constrained problem is that it might be infeasible.
         # hence we want some things that were originally in the constraints to be in the objective
         # Don't use ECOS since it's very confused
+
         grad_problem = Problem(Minimize(self.obj + obj))
         grad_problem.solve(
             solver=self.solver,
@@ -76,11 +77,10 @@ class Lamdba_Deriv_Problem_Wrapper:
                 verbose=VERBOSE,
                 warm_start=True,
             )
-            print "grad_problem.status", grad_problem.status, "value", grad_problem.value
-            assert(grad_problem.status in self.acceptable_status)
+            print "grad_problem.status (do again)", grad_problem.status, "value", grad_problem.value
 
         def _extract_values(var_vec):
-            return self.var_vec.value if self.var_vec is not None else 0
+            return var_vec.value if var_vec is not None else 0
 
         return {
             "dalphas_dlambda": [_extract_values(a) for a in self.dalphas_dlambda],
@@ -127,11 +127,10 @@ class Matrix_Completion_Groups_Hillclimb_Base(Gradient_Descent_Algo):
         betas = self.fmodel.current_model_params["betas"]
         gamma = self.fmodel.current_model_params["gamma"]
         u, s, v = self._get_svd_mini(gamma)
-        self.log("alphas %s" % alphas)
-        self.log("betas %s" % betas)
-        self.log("sigma %s" % np.diag(s))
-        # self.log("u %s" % u)
-        # self.log("v %s" % v)
+        self.log("model_deet alphas %s" % alphas)
+        self.log("model_deet betas %s" % betas)
+        self.log("model_deet sigma %s" % np.diag(s))
+        # self.log("model_deet gamma %s" % gamma)
 
         # check that the matrices are similar - sanity check
         self.log("data.real_matrix row 1 %s" % self.data.real_matrix[1,:])
@@ -193,9 +192,8 @@ class Matrix_Completion_Groups_Hillclimb_Base(Gradient_Descent_Algo):
                 v_hat,
                 lambdas,
             )
-            for k, v in grad_dict_i.iteritems():
-                self.log("grad_dict %d: %s %s" % (i, k, v))
-                print "grad_dict %d: %s %s" % (i, k, v)
+            # for k, v in grad_dict_i.iteritems():
+            #     self.log("grad_dict %d: %s %s" % (i, k, v))
             dval_dlambda_i = self._get_val_gradient(
                 grad_dict_i,
                 alphas,
@@ -237,18 +235,19 @@ class Matrix_Completion_Groups_Hillclimb_Base(Gradient_Descent_Algo):
         return u_mini, sigma_mini, v_mini
 
     def _get_minified_lambdas(self, lambdas, alpha_idxs, beta_idxs):
-        lambda_idxs = np.array(
-            [0] + alpha_idxs + beta_idxs
-        )
+        lambda_idxs = np.array(np.concatenate(
+            ([0], 1 + alpha_idxs, 1 + self.settings.num_row_groups + beta_idxs)
+        ), dtype=int)
+        print "lambda_idxs", lambda_idxs
         return lambda_idxs, lambdas[lambda_idxs]
 
-    def _get_nonzero_mini(self, alphas, betas):
+    def _get_nonzero_mini(self, vecs, feats):
         # return a smaller version of alpha and beta with zero elements removed
         # also returns a smaller version of the feature vectors
         vec_minis = []
         feat_minis = []
         idxs = []
-        for i, v_f_tuple in enumerate(zip(vec_minis, feat_minis)):
+        for i, v_f_tuple in enumerate(zip(vecs, feats)):
             v, f = v_f_tuple
             # get the mini mask
             v_mask = np.where(np.abs(v) > self.zero_thres)[0]
@@ -256,21 +255,22 @@ class Matrix_Completion_Groups_Hillclimb_Base(Gradient_Descent_Algo):
                 idxs.append(i)
                 vec_minis.append(np.reshape(v[v_mask], (v.size, 1)))
                 feat_minis.append(f[:, v_mask])
-        return vec_minis, feat_minis, idxs
+        return vec_minis, feat_minis, np.array(idxs)
 
     def _get_val_gradient(self, grad_dict, alphas, betas, gamma, row_features, col_features):
         # get gradient of the validation loss wrt lambda given the gradient of the
         # model parameters wrt lambda
         model_grad = grad_dict["dgamma_dlambda"]
-        for row_f, da_dlambda in zip(grad_dict["dalphas_dlambda"], row_features):
+        for da_dlambda, row_f in zip(grad_dict["dalphas_dlambda"], row_features):
             model_grad += row_f * da_dlambda * self.onesT_row
-        for col_f, db_dlambda in zip(grad_dict["dbetas_dlambda"], row_features):
+        for db_dlambda, col_f in zip(grad_dict["dbetas_dlambda"], col_features):
             model_grad += (col_f * db_dlambda * self.onesT_col).T
 
         dval_dlambda = - 1.0/self.num_val * (
             self.val_vec_diag
             * make_column_major_flat(
-                get_matrix_completion_groups_fitted_values(
+                self.data.observed_matrix
+                - get_matrix_completion_groups_fitted_values(
                     row_features,
                     col_features,
                     alphas,
@@ -312,32 +312,39 @@ class Matrix_Completion_Groups_Hillclimb_Base(Gradient_Descent_Algo):
         # imp_derivs should be a Lamdba_Deriv_Problem_Wrapper instance
         dd_square_loss = imp_derivs.dgamma_dlambda
         if len(imp_derivs.dalphas_dlambda) > 0:
-            dd_square_loss += hstack(row_features) * vstack(imp_derivs.dalphas_dlambda) * self.onesT_row
-        if len(imp_derivs.dalphas_dlambda) > 0:
-            dd_square_loss += (hstack(col_features) * vstack(imp_derivs.dbetas_dlambda) * self.onesT_col).T
+            dd_square_loss += np.hstack(row_features) * vstack(*imp_derivs.dalphas_dlambda) * self.onesT_row
+        if len(imp_derivs.dbetas_dlambda) > 0:
+            dd_square_loss += (np.hstack(col_features) * vstack(*imp_derivs.dbetas_dlambda) * self.onesT_col).T
         return 1.0/self.num_train * self.train_vec_diag * vec(dd_square_loss)
 
-    # def _double_check_derivative_indepth(self, lambda_idx, model1, model2, model0, eps):
-    #     # not everything should be zero, if it is not differentiable at that point
-    #     dalpha_dlambda = (model1["alpha"] - model2["alpha"])/(eps * 2)
-    #     dbeta_dlambda = (model1["beta"] - model2["beta"])/(eps * 2)
-    #
-    #     gamma1 = model1["gamma"]
-    #     u1, s1, v1 = self._get_svd_mini(gamma1)
-    #     gamma2 = model2["gamma"]
-    #     u2, s2, v2 = self._get_svd_mini(gamma2)
-    #     gamma0 = model0["gamma"]
-    #     u_hat, sigma_hat, v_hat = self._get_svd_mini(gamma0)
-    #     dU_dlambda = (u1 - u2)/(eps * 2)
-    #     dV_dlambda = (v1 - v2)/(eps*2)
-    #     dSigma_dlambda = (s1 - s2)/(eps * 2)
-    #     dgamma_dlambda = (gamma1 - gamma2)/(eps * 2)
-    #
-    #     print "dalpha_dlambda, %s" % (dalpha_dlambda)
-    #     print "dBeta_dlambda, %s" % (dbeta_dlambda)
-    #     print "dU_dlambda", dU_dlambda
-    #     print "ds_dlambda, %s" % (dSigma_dlambda)
-    #     # print "dgamma_dlambda, %s" % (dgamma_dlambda)
+    def _double_check_derivative_indepth(self, lambda_idx, model1, model2, model0, eps):
+        # if it is not differentiable at that point, grad not necessarily zero
+        print "alpha1 - deriv", model1["alphas"]
+        print "alpha2 - deriv", model2["alphas"]
+
+        if len(model1["alphas"]) > 0:
+            dalpha0_dlambda = (model1["alphas"][0] - model2["alphas"][0])/(eps * 2)
+            print "dalpha0_dlambda", dalpha0_dlambda
+        if len(model1["betas"]) > 0:
+            dbeta0_dlambda = (model1["betas"][0] - model2["betas"][0])/(eps * 2)
+            print "dbeta0_dlambda", dbeta0_dlambda
+
+
+        gamma1 = model1["gamma"]
+        u1, s1, v1 = self._get_svd_mini(gamma1)
+        gamma2 = model2["gamma"]
+        u2, s2, v2 = self._get_svd_mini(gamma2)
+        dU_dlambda = (u1 - u2)/(eps * 2)
+        dV_dlambda = (v1 - v2)/(eps*2)
+        dSigma_dlambda = (s1 - s2)/(eps * 2)
+        dgamma_dlambda = (gamma1 - gamma2)/(eps * 2)
+
+        # print "dalpha_dlambda, %s" % (dalpha_dlambda)
+        # print "dBeta_dlambda, %s" % (dbeta_dlambda)
+        print "ds_dlambda, %s" % (dSigma_dlambda)
+        print "dU_dlambda", dU_dlambda
+        print "dV_dlambda", dV_dlambda
+        # print "dgamma_dlambda, %s" % (dgamma_dlambda)
 
     def _check_optimality_conditions(self, model_params, lambdas, opt_thres=1e-2):
         # sanity check function to see that cvxpy is solving to a good enough accuracy
@@ -384,34 +391,40 @@ class Matrix_Completion_Groups_Hillclimb_Base(Gradient_Descent_Algo):
             )
             + lambdas[0] * np.sign(sigma_hat) * v_hat.T
         )
-        print "grad_at_opt wrt gamma (should be zero)", left_grad_at_opt_gamma, right_grad_at_opt_gamma
+        print "left grad_at_opt wrt gamma (should be zero)", get_norm2(left_grad_at_opt_gamma)
+        print "right grad_at_opt wrt gamma (should be zero)", get_norm2(right_grad_at_opt_gamma)
+        # assert(get_norm2(left_grad_at_opt_gamma) < opt_thres)
+        # assert(get_norm2(right_grad_at_opt_gamma) < opt_thres)
 
-        for alpha, row_f in zip(alphas, self.data.row_features):
+        for i, a_f_tuple in enumerate(zip(alphas, self.data.row_features)):
+            alpha, row_f = a_f_tuple
             print 'alpha', alpha
             if np.linalg.norm(alpha) > 1e-5:
                 grad_at_opt_alpha = []
-                for i in range(alpha.size):
+                for j in range(alpha.size):
                     grad_at_opt_alpha.append((
                         d_square_loss.T * make_column_major_flat(
-                            row_f[:, i] * self.onesT_row
+                            row_f[:, j] * self.onesT_row
                         )
-                        + lambdas[1 + i] * alpha[i]/np.linalg.norm(alpha, ord=None)
+                        + lambdas[1 + i] * alpha[j]/np.linalg.norm(alpha, ord=None)
                     )[0,0])
-                print "grad_at_opt wrt alpha (should be zero)", grad_at_opt_alpha
+                print "grad_at_opt wrt alpha (should be zero)", get_norm2(grad_at_opt_alpha)
+                # assert(np.linalg.norm(grad_at_opt_alpha) < opt_thres)
 
-        for beta, col_f in zip(betas, self.data.col_features):
+        for i, b_f_tuple in enumerate(zip(betas, self.data.col_features)):
+            beta, col_f = b_f_tuple
             print "beta", beta
             if np.linalg.norm(beta) > 1e-5:
                 grad_at_opt_beta = []
-                for i in range(beta.size):
+                for j in range(beta.size):
                     grad_at_opt_beta.append((
                         d_square_loss.T * make_column_major_flat(
-                            (col_f[:, i] * self.onesT_col).T
+                            (col_f[:, j] * self.onesT_col).T
                         )
-                        + lambdas[1 + self.settings.num_row_groups + i] * beta[i]/np.linalg.norm(beta, ord=None)
+                        + lambdas[1 + self.settings.num_row_groups + i] * beta[j]/np.linalg.norm(beta, ord=None)
                     )[0,0])
-                print "grad_at_opt wrt beta (should be zero)", grad_at_opt_beta
-        1/0
+                print "grad_at_opt wrt beta (should be zero)", get_norm2(grad_at_opt_beta)
+                # assert(get_norm2(grad_at_opt_beta) < opt_thres)
 
 class Matrix_Completion_Groups_Hillclimb(Matrix_Completion_Groups_Hillclimb_Base):
     method_label = "Matrix_Completion_Groups_Hillclimb"
@@ -486,28 +499,36 @@ class Matrix_Completion_Groups_Hillclimb(Matrix_Completion_Groups_Hillclimb_Base
         # alpha and beta, respectively
 
         constraints_dalpha = []
-        for row_f, alpha, da_dlambda in zip(row_features, alphas, imp_derivs.dalphas_dlambda):
-            for i in range(alpha.size):
+        for i, a_tuple in enumerate(zip(row_features, alphas, imp_derivs.dalphas_dlambda)):
+            row_f, alpha, da_dlambda = a_tuple
+            for j in range(alpha.size):
                 dalpha_imp_deriv_dlambda = (
-                    dd_square_loss.T * vec(row_f[:, i] * self.onesT_row)
-                    + lambdas[i + 1] * da_dlambda[i]
+                    dd_square_loss.T * vec(row_f[:, j] * self.onesT_row)
+                    + lambdas[i + 1] * (
+                        da_dlambda[j]/get_norm2(alpha, power=1)
+                        - alpha[j]/get_norm2(alpha, power=3) * (alpha.T * da_dlambda)
+                    )
                 )
                 if lambda_idx == i + 1:
-                    dalpha_imp_deriv_dlambda += alpha[i]/np.linalg.norm(alpha, ord=None)
+                    dalpha_imp_deriv_dlambda += alpha[j]/get_norm2(alpha, power=1)
                 constraints_dalpha.append(dalpha_imp_deriv_dlambda == 0)
                 obj += sum_squares(dalpha_imp_deriv_dlambda)
 
         constraints_dbeta = []
-        for col_f, beta, db_dlambda in zip(col_features, betas, imp_derivs.dbetas_dlambda):
-            for i in range(beta.size):
+        for i, b_tuple in enumerate(zip(col_features, betas, imp_derivs.dbetas_dlambda)):
+            col_f, beta, db_dlambda = b_tuple
+            for j in range(beta.size):
                 dbeta_imp_deriv_dlambda = (
                     dd_square_loss.T * vec(
-                        (col_f[:, i] * self.onesT_col).T
+                        (col_f[:, j] * self.onesT_col).T
                     )
-                    + lambdas[i + 1 + num_alphas] * imp_derivs.dbeta_dlambda[i]
+                    + lambdas[i + 1 + num_alphas] * (
+                        db_dlambda[j]/get_norm2(beta, power=1)
+                        - beta[j]/get_norm2(beta, power=3) * (beta.T * db_dlambda)
+                    )
                 )
                 if lambda_idx == i + 1 + num_alphas:
-                    dbeta_imp_deriv_dlambda += beta[i]/np.linalg.norm(beta, ord=None)
+                    dbeta_imp_deriv_dlambda += beta[j]/get_norm2(beta, power=1)
                 constraints_dbeta.append(dbeta_imp_deriv_dlambda == 0)
                 obj += sum_squares(dbeta_imp_deriv_dlambda)
 

code_submission_nonsmooth/matrix_completion_groups_solver.py
@@ -4,12 +4,14 @@ import numpy as np
 import scipy as sp
 from common import make_column_major_flat, make_column_major_reshape, print_time
 from common import get_matrix_completion_groups_fitted_values
+from common import get_norm2
 
 class MatrixCompletionGroupsProblem:
-    step_size = 0.8
+    step_size = 1.0
+    min_step_size = 1e-6
     step_size_shrink = 0.75
-    step_size_shrink_small = 0.1
-    print_iter = 1
+    step_size_shrink_small = 0.2
+    print_iter = 1000
 
     def __init__(self, data):
         self.num_rows = data.num_rows
@@ -53,7 +55,8 @@ class MatrixCompletionGroupsProblem:
         )
         return masked_obs_m
 
-    def get_value(self, alphas, betas, gamma):
+    # @print_time
+    def get_value(self, alphas, betas, gamma, gamma_nuc_norm=None):
         matrix_eval = make_column_major_flat(
             self.observed_matrix
             - get_matrix_completion_groups_fitted_values(
@@ -64,19 +67,25 @@ class MatrixCompletionGroupsProblem:
                 gamma,
             )
         )
-        square_loss = 0.5/self.num_train * np.power(np.linalg.norm(
+
+        square_loss = 0.5/self.num_train * get_norm2(
             matrix_eval[self.train_idx],
-            ord=None
-        ), 2)
-        nuc_norm = self.lambdas[0] * np.linalg.norm(gamma, ord="nuc")
+            power=2,
+        )
+
+        if gamma_nuc_norm is not None:
+            nuc_norm = self.lambdas[0] * gamma_nuc_norm
+        else:
+            nuc_norm = self.lambdas[0] * np.linalg.norm(gamma, ord="nuc")
+
         alpha_pen = 0
         for i, a in enumerate(alphas):
             # group lasso penalties
-            alpha_pen += self.lambdas[1 + i] * np.linalg.norm(a, ord=2)
+            alpha_pen += self.lambdas[1 + i] * get_norm2(a, power=1)
         beta_pen = 0
         for i, b in enumerate(betas):
             # group lasso penalties
-            beta_pen += self.lambdas[1 + self.num_row_groups + i] * np.linalg.norm(b, ord=2)
+            beta_pen += self.lambdas[1 + self.num_row_groups + i] * get_norm2(b, power=1)
         return square_loss + nuc_norm + alpha_pen + beta_pen
 
     def update(self, lambdas):
@@ -86,20 +95,19 @@ class MatrixCompletionGroupsProblem:
     def solve(self, max_iters=1000, tol=1e-5):
         start_time = time.time()
         step_size = self.step_size
-        old_val = None
+        old_val = self.get_value(
+            self.alphas_curr,
+            self.betas_curr,
+            self.gamma_curr
+        )
+        val_drop = 0
         for i in range(max_iters):
-            old_val = self.get_value(
-                self.alphas_curr,
-                self.betas_curr,
-                self.gamma_curr
-            )
-
             if i % self.print_iter == 0:
                 print "iter %d: cost %f time=%f (step size %f)" % (i, old_val, time.time() - start_time, step_size)
                 sys.stdout.flush()
 
             alphas_grad, betas_grad, gamma_grad = self.get_smooth_gradient()
-            potential_val, potential_alphas, potential_betas, potential_gamma = self._get_potential_values(
+            potential_val, potential_alphas, potential_betas, potential_gamma = self.get_potential_values(
                 step_size,
                 alphas_grad,
                 betas_grad,
@@ -107,32 +115,36 @@ class MatrixCompletionGroupsProblem:
             )
             while old_val < potential_val and step_size > self.min_step_size:
                 print "potential val is bigger: %f < %f" % (old_val, potential_val)
-                step_size *= self.step_size_shrink
                 if potential_val > 10 * old_val:
                     step_size *= self.step_size_shrink_small
-                potential_val, potential_alphas, potential_betas, potential_gamma = self._get_potential_values(
+                else:
+                    step_size *= self.step_size_shrink
+                potential_val, potential_alphas, potential_betas, potential_gamma = self.get_potential_values(
                     step_size,
                     alphas_grad,
                     betas_grad,
                     gamma_grad,
                 )
 
-            if old_val > potential_val:
+            val_drop = old_val - potential_val
+            if old_val >= potential_val:
                 self.alphas_curr = potential_alphas
                 self.betas_curr = potential_betas
                 self.gamma_curr = potential_gamma
-                if old_val - potential_val < tol:
+                old_val = potential_val
+                if val_drop < tol:
                     print "decrease is too small"
                     break
             else:
                 print "step size too small. increased in cost"
                 break
-        print "solver diff (log10) %f" % np.log10(old_val - potential_val)
+        print "fin cost %f, solver diff (log10) %f, steps %d" % (old_val, np.log10(val_drop), i)
         return self.alphas_curr, self.betas_curr, self.gamma_curr
 
-    def _get_potential_values(self, step_size, alphas_grad, betas_grad, gamma_grad):
+    # @print_time
+    def get_potential_values(self, step_size, alphas_grad, betas_grad, gamma_grad):
         try:
-            potential_gamma, num_nonzero_sv = self.get_prox_nuclear(
+            potential_gamma, potential_nuc_norm, num_nonzero_sv = self.get_prox_nuclear(
                 self.gamma_curr - step_size * gamma_grad,
                 step_size * self.lambdas[0]
             )
@@ -140,21 +152,25 @@ class MatrixCompletionGroupsProblem:
             print "SVD did not converge - ignore proximal gradient step for nuclear norm"
             potential_gamma = self.gamma_curr - step_size * gamma_grad
 
-        potential_alphas = []
-        for i, a_g_tuple in enumerate(zip(self.alphas_curr, alphas_grad)):
-            a, g = a_g_tuple
-            potential_alphas.append(self.get_prox_l2(
-                a - step_size * g,
+        # s = time.time()
+
+        potential_alphas = [
+            self.get_prox_l2(
+                a_g_tuple[0] - step_size * a_g_tuple[1],
                 step_size * self.lambdas[1 + i]
-            ))
-        potential_betas = []
-        for i, b_g_tuple in enumerate(zip(self.betas_curr, betas_grad)):
-            b, g = b_g_tuple
-            potential_betas.append(self.get_prox_l2(
-                b - step_size * g,
+            )
+            for i, a_g_tuple in enumerate(zip(self.alphas_curr, alphas_grad))
+        ]
+        potential_betas = [
+            self.get_prox_l2(
+                b_g_tuple[0] - step_size * b_g_tuple[1],
                 step_size * self.lambdas[1 + self.num_row_groups + i]
-            ))
-        potential_val = self.get_value(potential_alphas, potential_betas, potential_gamma)
+            )
+            for i, b_g_tuple in enumerate(zip(self.betas_curr, betas_grad))
+        ]
+
+        # print "prox alpha beta time", time.time() - s
+        potential_val = self.get_value(potential_alphas, potential_betas, potential_gamma, gamma_nuc_norm=potential_nuc_norm)
         return potential_val, potential_alphas, potential_betas, potential_gamma
 
     # @print_time
@@ -194,20 +210,24 @@ class MatrixCompletionGroupsProblem:
             beta_grad = (d_square_loss.T *  col_features_one).T
             return beta_grad
 
+        # s = time.time()
         alphas_grad = [_get_alpha_grad(f) for f in self.row_features]
         betas_grad = [_get_beta_grad(f) for f in self.col_features]
+        # print "alpha beta grad time", time.time() - s
         return alphas_grad, betas_grad, _get_gamma_grad()
 
     # @print_time
     # This is the time sink! Can we make this faster?
+    # @returns soln_to_prox, nuc_norm of soln_to_prox, and number of nonzero singular vals
     def get_prox_nuclear(self, x_matrix, scale_factor):
         # prox of function scale_factor * nuclear_norm
         u, s, vt = np.linalg.svd(x_matrix, full_matrices=False)
         thres_s = np.maximum(s - scale_factor, 0)
+        nuc_norm = np.linalg.norm(thres_s, ord=1)
         num_nonzero = (np.where(thres_s > 0))[0].size
-        return u * np.diag(thres_s) * vt, num_nonzero
+        return u * np.diag(thres_s) * vt, nuc_norm, num_nonzero
 
     # @print_time
     def get_prox_l2(self, x_vector, scale_factor):
-        thres_x = max(1 - scale_factor/np.linalg.norm(x_vector, ord=None), 0) * x_vector
+        thres_x = max(1 - scale_factor/get_norm2(x_vector, power=1), 0) * x_vector
         return thres_x