71d87c7174fbeac4406084b05c2c12db9f0328f5
Jean Feng <jean.feng@gmail.com>
2017-02-11 06:29:01 -0800

    update pdfs and reviewer edits from jj

--- code_submission_nonsmooth/matrix_completion_groups_eval.py
+++ code_submission_nonsmooth/matrix_completion_groups_eval.py
@@ -181,10 +181,10 @@ def fit_data_for_iter(iter_data):
     settings = iter_data.settings
 
     one_vec_all = np.ones(1 + settings.num_row_groups + settings.num_col_groups)
-    initial_lambdas_set = [one_vec_all * 0.1, one_vec_all * 0.05] #, one_vec_all * 0.003]
+    initial_lambdas_set = [one_vec_all * 0.005, one_vec_all * 0.003]
 
     one_vec2_all = np.ones(2)
-    simple_initial_lambdas_set = [one_vec2_all * 0.01, one_vec2_all * 0.005]
+    simple_initial_lambdas_set = [one_vec2_all * 0.005, one_vec2_all * 0.003]
 
     method = iter_data.settings.method
 
